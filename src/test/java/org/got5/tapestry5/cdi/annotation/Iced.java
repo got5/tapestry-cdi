c57c0188f7c4a0021d898589fa12c1c1bef35071
Nouredine Khadri <nourredine.k@gmail.com>
2013-05-15 12:01:46 +0200

    add license header

--- src/test/java/org/got5/tapestry5/cdi/annotation/Iced.java
+++ src/test/java/org/got5/tapestry5/cdi/annotation/Iced.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2013 GOT5
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.got5.tapestry5.cdi.annotation;
 
 import static java.lang.annotation.ElementType.PARAMETER;
@@ -9,11 +24,6 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Target;
 import javax.inject.Qualifier;
 
-/**
- * User: pierremarot
- * Date: 29/04/13
- * Time: 18:11
- */
 @Qualifier
 @Retention(RUNTIME)
 @Target({TYPE,METHOD,FIELD,PARAMETER})
--- src/test/java/org/got5/tapestry5/cdi/test/pages/DessertPage.java
+++ src/test/java/org/got5/tapestry5/cdi/test/pages/DessertPage.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2013 GOT5
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.got5.tapestry5.cdi.test.pages;
 
 import org.got5.tapestry5.cdi.annotation.Choco;
@@ -10,11 +25,6 @@ import javax.inject.Inject;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-/**
- * User: pierremarot
- * Date: 30/04/13
- * Time: 18:20
- */
 public class DessertPage {
 
     @Inject
